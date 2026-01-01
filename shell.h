682d4f8f2875f6b2ef1cc9e47ea0ee21295ad785
poloelo <p.gioria2304@gmail.com>
2026-01-01 01:41:31 +0100

    Adding header to encapsulate the functions

---
+++ shell.h
@@ -0,0 +1,41 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#define MAX_INPUT 1024
+#define MAX_ARGS 64
+
+extern char **environ;
+
+/* Main shell functions */
+void display_prompt(void);
+char *read_line(void);
+char **parse_line(char *line);
+int execute_command(char **args, char *shell_name, int line_count);
+
+/* Built-in functions */
+int shell_exit(char **args);
+int shell_env(char **args);
+int check_builtin(char **args);
+
+/* Path handling */
+char *find_in_path(char *command);
+char *get_path_env(void);
+
+/* Helper functions */
+void print_error(char *shell_name, int line_count, char *command);
+void free_array(char **array);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+
+#endif /* SHELL_H */
