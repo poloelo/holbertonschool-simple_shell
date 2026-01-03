7c9ef061d309f3c343c710822b459c9c7654215d
poloelo <p.gioria2304@gmail.com>
2026-01-03 02:52:44 +0100

    betty correction

--- execute.c
+++ execute.c
@@ -1,6 +1,43 @@
 #include "shell.h"
 
 /**
+ * get_command_path - Get full path of command
+ * @args: Array of arguments
+ *
+ * Return: Full path to command or NULL
+ */
+char *get_command_path(char **args)
+{
+	char *command = NULL;
+
+	if (args[0][0] == '/' || args[0][0] == '.')
+		command = args[0];
+	else
+		command = find_in_path(args[0]);
+
+	return (command);
+}
+
+/**
+ * execute_child - Execute command in child process
+ * @command: Command path to execute
+ * @args: Array of arguments
+ * @shell_name: Name of the shell
+ *
+ * Return: Does not return (exits)
+ */
+void execute_child(char *command, char **args, char *shell_name)
+{
+	if (execve(command, args, environ) == -1)
+	{
+		perror(shell_name);
+		if (command != args[0])
+			free(command);
+		exit(127);
+	}
+}
+
+/**
  * execute_command - Execute a command
  * @args: Array of arguments
  * @shell_name: Name of the shell (argv[0])
@@ -15,10 +52,7 @@ int execute_command(char **args, char *shell_name, int line_count)
 	char *command = NULL;
 	struct stat st;
 
-	if (args[0][0] == '/' || args[0][0] == '.')
-		command = args[0];
-	else
-		command = find_in_path(args[0]);
+	command = get_command_path(args);
 
 	if (command == NULL || stat(command, &st) != 0)
 	{
@@ -38,15 +72,7 @@ int execute_command(char **args, char *shell_name, int line_count)
 	}
 
 	if (pid == 0)
-	{
-		if (execve(command, args, environ) == -1)
-		{
-			perror(shell_name);
-			if (command != args[0])
-				free(command);
-			exit(127);
-		}
-	}
+		execute_child(command, args, shell_name);
 	else
 	{
 		waitpid(pid, &status, 0);
@@ -58,4 +84,3 @@ int execute_command(char **args, char *shell_name, int line_count)
 
 	return (0);
 }
-
