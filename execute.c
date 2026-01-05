ba2d72f425cdf8e6769210038d2945a7e8c36ace
sam99-web <keishsam99@gmail.com>
2026-01-05 11:54:18 +0000

    new commit fille executetask5

--- execute.c
+++ execute.c
@@ -1,86 +1,98 @@
 #include "shell.h"
 
 /**
- * get_command_path - Get full path of command
+ * get_command_path - Get full path of a command
  * @args: Array of arguments
  *
- * Return: Full path to command or NULL
+ * Return: Full path of the command or NULL if not found
+ * Note: Always returns a dynamically allocated string that must be freed
  */
 char *get_command_path(char **args)
 {
-	char *command = NULL;
+    char *command = NULL;
 
-	if (args[0][0] == '/' || args[0][0] == '.')
-		command = args[0];
-	else
-		command = find_in_path(args[0]);
+    if (args[0][0] == '/' || args[0][0] == '.')
+        command = _strdup(args[0]);  /* duplicate for safe free */
+    else
+        command = find_in_path(args[0]); /* already malloc'd */
 
-	return (command);
+    return command;
 }
 
 /**
- * execute_child - Execute command in child process
- * @command: Command path to execute
+ * execute_child - Execute a command in child process
+ * @command: Full path to command
  * @args: Array of arguments
- * @shell_name: Name of the shell
- *
- * Return: Does not return (exits)
+ * @shell_name: Name of the shell for error messages
  */
 void execute_child(char *command, char **args, char *shell_name)
 {
-	if (execve(command, args, environ) == -1)
-	{
-		perror(shell_name);
-		if (command != args[0])
-			free(command);
-		exit(127);
-	}
+    if (access(command, X_OK) != 0) /* Check execution permission */
+    {
+        write(STDERR_FILENO, shell_name, _strlen(shell_name));
+        write(STDERR_FILENO, ": Permission denied\n", 20);
+        exit(126); /* Standard code for permission denied */
+    }
+
+    if (execve(command, args, environ) == -1)
+    {
+        perror(shell_name); /* fallback perror for other errors */
+        exit(127); /* command not found */
+    }
 }
 
 /**
- * execute_command - Execute a command
+ * execute_command - Execute a command (builtin or external)
  * @args: Array of arguments
  * @shell_name: Name of the shell (argv[0])
- * @line_count: Current line number
+ * @line_count: Current line number for error messages
  *
- * Return: Status of execution
+ * Return: Status code of the executed command
  */
 int execute_command(char **args, char *shell_name, int line_count)
 {
-	pid_t pid;
-	int status;
-	char *command = NULL;
-	struct stat st;
+    pid_t pid;
+    int status;
+    char *command = NULL;
+    struct stat st;
 
-	command = get_command_path(args);
+    if (args[0] == NULL)
+        return 0; /* Nothing to execute */
 
-	if (command == NULL || stat(command, &st) != 0)
-	{
-		print_error(shell_name, line_count, args[0]);
-		if (command != NULL && command != args[0])
-			free(command);
-		return (127);
-	}
+    command = get_command_path(args);
+    if (command == NULL || stat(command, &st) != 0)
+    {
+        print_error(shell_name, line_count, args[0]);
+        if (command != NULL)
+            free(command);
+        return 127; /* command not found */
+    }
 
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		if (command != args[0])
-			free(command);
-		return (1);
-	}
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        free(command);
+        return 1; /* fork failed */
+    }
 
-	if (pid == 0)
-		execute_child(command, args, shell_name);
-	else
-	{
-		waitpid(pid, &status, 0);
-		if (command != args[0])
-			free(command);
-		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
-	}
+    if (pid == 0) /* Child process */
+    {
+        execute_child(command, args, shell_name);
+        /* child exits inside execute_child */
+    }
+    else /* Parent process */
+    {
+        waitpid(pid, &status, 0);
 
-	return (0);
+        if (command != NULL)
+            free(command);
+
+        if (WIFEXITED(status))
+            return WEXITSTATUS(status);
+        else if (WIFSIGNALED(status))
+            return 128 + WTERMSIG(status); /* terminated by signal */
+    }
+
+    return 0;
 }
