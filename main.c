0589998507d901ed29bafd59a11deb94f4eaa168
poloelo <p.gioria2304@gmail.com>
2026-01-08 12:12:28 +0100

    Correction of the main.c file

--- main.c
+++ main.c
@@ -1,6 +1,46 @@
 #include "shell.h"
 
 /**
+ * process_line - Process a single line of input
+ * @line: Line to process
+ * @av: Argument vector
+ * @line_count: Current line count
+ *
+ * Return: Status of execution
+ */
+int process_line(char *line, char **av, int line_count)
+{
+	char **args = NULL;
+	int status = 0;
+
+	if (line[0] == '\0' || line[0] == '\n')
+	{
+		free(line);
+		return (0);
+	}
+
+	args = parse_line(line);
+	free(line);
+
+	if (args == NULL || args[0] == NULL)
+	{
+		free_array(args);
+		return (0);
+	}
+
+	if (check_builtin(args) == 0)
+	{
+		free_array(args);
+		return (0);
+	}
+
+	status = execute_command(args, av[0], line_count);
+	free_array(args);
+
+	return (status);
+}
+
+/**
  * main - Entry point for simple shell
  * @ac: Argument count
  * @av: Argument vector
@@ -10,7 +50,6 @@
 int main(int ac, char **av)
 {
 	char *line = NULL;
-	char **args = NULL;
 	int status = 0;
 	int line_count = 0;
 	(void)ac;
@@ -30,29 +69,7 @@ int main(int ac, char **av)
 			break;
 		}
 
-		if (line[0] == '\0' || line[0] == '\n')
-		{
-			free(line);
-			continue;
-		}
-
-		args = parse_line(line);
-		free(line);
-
-		if (args == NULL || args[0] == NULL)
-		{
-			free_array(args);
-			continue;
-		}
-
-		if (check_builtin(args) == 0)
-		{
-			free_array(args);
-			continue;
-		}
-
-		status = execute_command(args, av[0], line_count);
-		free_array(args);
+		status = process_line(line, av, line_count);
 	}
 
 	return (status);
