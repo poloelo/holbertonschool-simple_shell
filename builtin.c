f69109c726b05fdfa72a4d76143e04310654fde2
poloelo <p.gioria2304@gmail.com>
2026-01-08 11:55:47 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,15 +1,15 @@
 #include "shell.h"
 
 /**
- * shell_exit - Exit the shell
+ * shell_exit - Signal to exit the shell
  * @args: Array of arguments
  *
- * Return: -1 to signal exit
+ * Return: -1 to signal exit to caller
  */
 int shell_exit(char **args)
 {
-	(void)args;
-	exit(0);
+    (void)args;
+    return (-1);  // NE PAS appeler exit() ici, juste retourner -1
 }
 
 /**
@@ -20,17 +20,18 @@ int shell_exit(char **args)
  */
 int shell_env(char **args)
 {
-	int i = 0;
-	(void)args;
-
-	while (environ[i] != NULL)
-	{
-		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
-		write(STDOUT_FILENO, "\n", 1);
-		i++;
-	}
-
-	return (0);
+    int i = 0;
+    
+    (void)args;
+    
+    while (environ[i] != NULL)
+    {
+        write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+        write(STDOUT_FILENO, "\n", 1);
+        i++;
+    }
+    
+    return (0);
 }
 
 /**
@@ -41,19 +42,23 @@ int shell_env(char **args)
  */
 int check_builtin(char **args)
 {
-	int i;
-	char *builtins[] = {"exit", "env", NULL};
-
-	int (*builtin_funcs[])(char **) = {shell_exit, shell_env};
-	for (i = 0; builtins[i] != NULL; i++)
-	{
-		if (_strcmp(args[0], builtins[i]) == 0)
-		{
-			if (builtin_funcs[i](args) == -1)
-				exit(EXIT_SUCCESS);
-			return (0);
-		}
-	}
-
-	return (1);
+    int i;
+    int status;
+    char *builtins[] = {"exit", "env", NULL};
+    int (*builtin_funcs[])(char **) = {shell_exit, shell_env};
+    
+    for (i = 0; builtins[i] != NULL; i++)
+    {
+        if (_strcmp(args[0], builtins[i]) == 0)
+        {
+            status = builtin_funcs[i](args);
+            
+            if (status == -1)  // shell_exit a retourné -1
+                return (-1);    // Propager le signal au main
+            
+            return (0);  // Builtin exécuté avec succès
+        }
+    }
+    
+    return (1);  // Pas un builtin
 }
